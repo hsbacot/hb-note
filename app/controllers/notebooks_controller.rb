857e512dec8c429c8808488c0fa1ca0ea08a4c4b
hsbacot <hsbacot@gmail.com>
2013-07-30 08:36:57 -0400

    notebook actions working

--- app/controllers/notebooks_controller.rb
+++ app/controllers/notebooks_controller.rb
@@ -4,6 +4,7 @@ class NotebooksController < ApplicationController
   end
 
   def show
+    @notebook = Notebook.find(params[:id])
   end
 
   def new
@@ -11,15 +12,34 @@ class NotebooksController < ApplicationController
   end
 
   def edit
+    @notebook = current_user.notebooks.find(params[:id])
   end
 
   def create
-
+    @notebook = current_user.notebooks.new(notebook_params)
+    if @notebook.save
+      redirect_to @notebook, notice: "Notebook successfully created"
+    else
+      redirect_to root_path
+    end
   end
 
   def update
+    @notebook = current_user.notebooks.find(params[:id])
+    if @notebook.update_attributes(notebook_params)
+      redirect_to @notebook, notice: "Notebook successfully updated"
+    else
+      redirect_to root_path, notice: "Error updating notebook"
+    end
   end
 
   def destroy
+    @notebook = current_user.notebooks.find(params[:id])
+    @notebook.destroy
   end
+
+  private
+    def notebook_params
+      params.require(:notebook).permit(:name, :description)
+    end
 end
